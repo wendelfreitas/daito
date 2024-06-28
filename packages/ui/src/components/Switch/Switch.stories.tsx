af22f3cd155087476b261760af46bc05f9dd95aa
Wendel Freitas <wendelfreitasbranco@gmail.com>
2024-06-28 14:05:29 -0300

    feat: create switch component

---
+++ packages/ui/src/components/Switch/Switch.stories.tsx
@@ -0,0 +1,27 @@
+import { StoryFn, Meta } from '@storybook/react';
+import { Switch } from './Switch';
+import { useState } from 'react';
+import { Label } from '../Label/Label';
+
+const meta: Meta = {
+  title: 'Components/Switch',
+  component: Switch,
+};
+
+export default meta;
+
+export const Default: StoryFn = () => {
+  const [active, setActive] = useState(true);
+
+  return (
+    <div className="flex items-center space-x-2">
+      <Switch
+        checked={active}
+        onCheckedChange={setActive}
+        id="super-saiyan-mode"
+        aria-label="Super Saiyan mode"
+      />
+      <Label htmlFor="super-saiyan-mode">Super Saiyan mode</Label>
+    </div>
+  );
+};
