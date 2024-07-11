0896d47e5ea2df4a70c052597269b21c50e779c8
Wendel Freitas <wendelfreitasbranco@gmail.com>
2024-07-11 20:47:41 -0300

    feat: add react-context-menu package and change theme

--- packages/ui/.jest/setup.ts
+++ packages/ui/.jest/setup.ts
@@ -3,3 +3,14 @@ import '@testing-library/jest-dom';
 import '@testing-library/jest-dom/jest-globals';
 
 global.ResizeObserver = ResizeObserver;
+
+(global as any).DOMRect = {
+  fromRect: () => ({
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0,
+    width: 0,
+    height: 0,
+  }),
+};
