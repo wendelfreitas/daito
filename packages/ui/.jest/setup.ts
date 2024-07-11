c947d302c77aee91fba6c08f8fdd7658c3da2ef4
Wendel Freitas <wendelfreitasbranco@gmail.com>
2024-07-11 20:30:32 -0300

    feat: add resize observer for tests

--- packages/ui/.jest/setup.ts
+++ packages/ui/.jest/setup.ts
@@ -1,2 +1,5 @@
+import ResizeObserver from 'resize-observer-polyfill';
 import '@testing-library/jest-dom';
 import '@testing-library/jest-dom/jest-globals';
+
+global.ResizeObserver = ResizeObserver;
